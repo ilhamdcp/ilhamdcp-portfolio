b39bfe671d865641ab190c320304e7cdaa15e5b6
Ilham Darmawan CP <ilhamdcp@gmail.com>
2019-04-21 21:36:19 +0700

    Initialize react app

---
+++ src/Description.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+class Description extends Component {
+  render() {
+    return (
+      <div className="Description">
+      </div>
+    );
+  }
+}
+
+export default App;
